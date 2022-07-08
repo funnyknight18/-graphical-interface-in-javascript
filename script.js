cf678baebb1ddfc13accf7da40f3f30f0fff20bd
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-07-07 20:38:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,37 +7,30 @@ const  $button = document.querySelector('[data-js="button-logIn"]');
 const  $contact = document.querySelector('[data-js="contact"]'); 
 const  $button_contact = document.querySelector('[data-js="button-contact"]'); 
 const  $inputEstate = document.querySelector('[data-js="ddd_state"]');  
+const $button_finsh = document.querySelector('[data-js="button-finsh"]'); 
 
 
 
 $button.addEventListener( 'click' , Button_Of_Send , false );
-$button_contact.addEventListener('click' , sendNumberContact, false );
+$button_contact.addEventListener('click' , clickNumber, false );
+$button_finsh.addEventListener('click' , twoModule , false ); 
 
-
-
-function aceptUser() { 
-   window.alert('Bem Vindo!');
-}
-
-aceptUser(); 
 
 
 function isSubmit() {
   let email = $email.value; 
   let password = $password.value; 
-  let contatc = $contact.value; 
 
- if(  email === '' || password === '' ||  contatc === ''){
-   window.alert('Preenchar todas as informaçoes'); }
- else{ window.alert(`Seu dados... Senha: (${$password.value}) email: ${$email.value} contact ${ $contact.value}`);
- }     
+ if(  email === '' || password === '' )
+   window.alert('Preenchar todas as informaçoes');    
+   
+  console.log('senha:' , password , 'email:', email  );   
 }
 
 
 function clearInputs () { 
   $email.value = '';  
   $password.value = ''; 
-  $contact.value = ''; 
 }
 
 function  Button_Of_Send () { 
@@ -45,7 +38,22 @@ function  Button_Of_Send () {
   clearInputs(); // limpar logo apos o inputs 
 }
 
-function sendNumberContact() { 
+
+
+//  essa é a 2  interaçaõ, para ve se o usuario preencheu todas as informaçoes... 
+
+
+
+
+function isVeryficInputNumber() {  // verificar se os inputs estam preenchidos... 
+  let DDD =  $inputEstate.value; 
+  let contatc = $contact.value; 
+DDD === '' || contatc === '' ?  window.alert('Preenchar todas as informaçoes'): 'contato prenechido'; 
+}
+
+
+
+function sendNumberContact() { // saber qual regiao do usuario... 
   let state = $inputEstate.value ; 
   let DDD ;   
  switch (state) {
@@ -58,34 +66,37 @@ function sendNumberContact() {
   case 'AP': 
    DDD = '(91)';  
    break;  
+  case 'RJ': 
+   DDD = '(21)'; 
+   break;
+  case 'AC' : 
+   DDD = '(68)'
    case  undefined:
   default:
     window.alert('Nao Encotramos Seu DDD'); 
     break;
  }
  $contact.value =  DDD + ' ' +  $contact.value;  
-  
- if( DDD === '(92)')
-    window.alert('entao voce é um manaurara... :)');  
-
-    isnumberTrue();  
 }   
 
-function isnumberTrue() { 
-  var contato =  $contact.value; 
 
-  if(contato.match(/\d+/g)){ 
-    console.log('erro');
-  } else { 
-    console.log('great test'); 
-  }   
+function clickNumber() { 
+  sendNumberContact(); 
+  isVeryficInputNumber();  
 }
 
 
-function submitButton () { 
-  isSubmit(); 
-  clearInputs();
+
+ function twoModule() { 
+   var divCenter = document.querySelector('[data-js="center"]'); 
+   var divContact = document.querySelector('[data-js="contact-center"]'); 
+     
+    divCenter.innerHTML += '<h1>Deu certo </h1>'; 
+    divContact.innerHTML = '<h2>deu certo o outro modulo</h2>'
 }
 
 
 })( document , window);
+
+
+
