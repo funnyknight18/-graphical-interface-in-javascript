c3a1f19de3fbedfca8f4f39d2226c1cfd8d1db0b
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-07-20 12:05:45 -0400

    Create scriptSecondPage.js

---
+++ scriptSecondPage.js
@@ -0,0 +1,13 @@
+(function () { 
+    'use strict'; 
+ 
+      
+const $coutry = document.querySelector('[data-js="coutry"]'); 
+const $state = document.querySelector('[data-js="state"]'); 
+const $city = document.querySelector('[data-js="city"]'); 
+const $square = document.querySelector('[data-js="bairro"]');   
+const $cep = document.querySelector('[data-js="home"]');    
+const $numberHome = document.querySelector('[ data-js="home"]'); 
+
+
+} )(); 
