96ef76738e8c261064b5c765066e0385ecfb9770
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-07-21 15:27:50 -0400

    Update scriptSecondPage.js

--- scriptSecondPage.js
+++ scriptSecondPage.js
@@ -8,6 +8,9 @@ const $city = document.querySelector('[data-js="city"]');
 const $square = document.querySelector('[data-js="bairro"]');   
 const $cep = document.querySelector('[data-js="home"]');    
 const $numberHome = document.querySelector('[ data-js="home"]'); 
+const $button_singIn = document.querySelector('[data-js="button_sing"]'); 
+
+$button_singIn.addEventListener('click');
 
 
 } )(); 
